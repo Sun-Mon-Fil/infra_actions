0c60d572853786720b46b0c9003d751893acac87
Денис Филиппов <denbfil@yandex.ru>
2023-05-02 11:01:43 +0300

    deploy v5

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось что-то!')
 
 
 def second_page(request):
